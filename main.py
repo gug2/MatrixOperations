36245486cb827f3014582b0f3d160912478ce5a5
Andrey <andrei.smirnikov@yandex.ru>
2021-09-17 15:07:40 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,118 @@
 '''
-
 Welcome to GDB Online.
 GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
 C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
 Code, Compile, Run and Debug online from anywhere in world.
-
 '''
+
+' UTIL METHODS '
 def E(size):
-    e = []
+    matrix = []
+    
     k = 0
     for i in range(size):
-        e.append([int(j == k) for j in range(size)])
+        matrix.append([int(j == k) for j in range(size)])
         k += 1
-        
-    return e
-
-def removeRow(matrix, iR):
-    newMatrix = []
     
-    for i in range(len(matrix)):
-        if(i != iR):
-            newMatrix.append([matrix[i][j] for j in range(len(matrix[0]))])
+    return matrix
     
-    return newMatrix
-    
-def removeColumn(matrix, jR):
-    newMatrix = []
-    
-    for i in range(len(matrix)):
-        row = []
-        for j in range(len(matrix[0])):
-            if j != jR:
-                row.append(matrix[i][j])
-        newMatrix.append(row)
-    
-    return newMatrix
-
-def minor(matrix):
-    minor = 0
-    
-    if len(matrix) != len(matrix[0]):
-        print('error! matrix isn\'t square!')
-        return matrix
-        
-    if(len(matrix) == 1):
-        minor = matrix[0][0]
-    if(len(matrix) == 2):
-        minor = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    if(len(matrix) == 3):
-        minor += matrix[0][0] * matrix[1][1] * matrix[2][2]
-        minor += matrix[1][0] * matrix[2][1] * matrix[0][2]
-        minor += matrix[0][1] * matrix[1][2] * matrix[2][0]
-        minor -= matrix[0][2] * matrix[1][1] * matrix[2][0]
-        minor -= matrix[0][1] * matrix[1][0] * matrix[2][2]
-        minor -= matrix[1][2] * matrix[2][1] * matrix[0][0]
-    else:
-        print('error! size > 3!', 'len matrix:', len(matrix))
-        
-    return minor
-
-def det(matrix):
-    detA = 0
-    if len(matrix) != len(matrix[0]):
-        print('error! matrix isn\'t square!')
-        return matrix
-        
-    for j in range(len(matrix[0])):
-        A0j = -1**(1+j+1) * minor(removeRow(removeColumn(matrix, j), 0))
-        detA += matrix[0][j] * A0j
-        
-    return detA
-
-def transponse(matrix):
-    newMatrix = []
-    for j in range(len(matrix[0])):
-        newMatrix.append([matrix[i][j] for i in range(len(matrix))])
-            
-    return newMatrix
-
 def printm(matrix):
-    print('--matrix', len(matrix), 'x', len(matrix[0]), '--')
+    mHead(matrix)
+    
     for i in range(len(matrix)):
         print('[', end=' ')
         for j in range(len(matrix[0])):
             print(matrix[i][j], end=' ')
         print(']')
 
-print('enter size of matrix in format \'m x n\'')
-m, n = [int(a) for a in input().split()]
+def mHead(matrix):
+    width = len(matrix[0]) * 2 + 2 - 6
+    
+    for i in range(width // 2):
+        print('-', end='')
+    print('matrix', end='')
+    for i in range(width // 2 + 1):
+        print('-', end='')
+    
+    print()
+    
+' OPERATION METHODS '
+def inputm(inputStr):
+    matrix = []
+    
+    width = int(inputStr[0])
+    height = int(inputStr[1])
+    
+    for i in range(height):
+        rowStr = input().split()
+        if(len(rowStr) == width):
+            matrix.append([int(k) for k in rowStr])
+        
+    return matrix
+    
+def transponse(matrix):
+    newMatrix = []
+    
+    for j in range(len(matrix)):
+        newMatrix.append([matrix[i][j] for i in range(len(matrix[0]))])
+        
+    return newMatrix
+    
+def removeRow(matrix, row):
+    newMatrix = []
+    
+    for i in range(len(matrix)):
+        if i != row:
+            newMatrix.append([matrix[i][j] for j in range(len(matrix[0]))])
+            
+    return newMatrix
+    
+def removeColumn(matrix, column):
+    newMatrix = []
+    
+    for i in range(len(matrix)):
+        row = []
+        for j in range(len(matrix[0])):
+            if j != column:
+                row.append(matrix[i][j])
+        newMatrix.append(row)
+        
+    return newMatrix
+    
+def det(matrix):
+    detA = 0
+    
+    for j in range(len(matrix[0])):
+        print(len(matrix[0]))
+        printm(matrix)
+        
+        M = 0
+        if len(matrix[0]) == 1:
+            M = matrix[0][0]
+            print('endless M')
+            return M
+        else:
+            M = det(removeRow(removeColumn(matrix, j), 0))
+            print('progressive M')
+            A = ((-1)**(1+j+1)) * M
+            detA += matrix[0][j] * A
+    
+    return detA
 
-matrix = []
+' MAIN CODE '
+print('enter the size of matrix:')
 
-for i in range(m):
-    print('print ', i, ' line: ', end='')
-    matrix.append([int(a) for a in input().split()])
+matrix = [
+    [ 4, 2, 7, 6, 5, 4, 23 ],
+    [ 7, 7, 7, 0, 0, 4, 3 ],
+    [ 7, -9, 0, 6, 0, 5, 1 ],
+    [ 8, 8, -5, 0, 0, 5, 2 ],
+    [ 2, 9, 0, -3, 0, 4, 2 ],
+    [ 0, 1, 0, 4, -3, -4, 1 ],
+    [ 0, 0, 3, 0, 0, -2, 3 ]
+]
+'inputm(input().split())'
 
 printm(matrix)
+
 print(det(matrix))
